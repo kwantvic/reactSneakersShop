1abcee6bd7926bcab784d5a178261071eebaa705
kwant <user@macBookAir-kwant.local>
2021-09-03 10:00:57 +0300

    verstka+reactComponetsStart

---
+++ src/components/Drawer.js
@@ -0,0 +1,57 @@
+function Drawer() {
+  return (
+    <div style={{ display: 'none' }} className="overlay">
+      <div className="drawer">
+        <h2 className="mb-30 d-flex justify-between">
+          Корзина
+          <img className="removeBtn cu-p" src="/img/btn-remove.svg" alt="Remove" />
+        </h2>
+
+        <div className="items">
+          <div className="cartItem d-flex align-center mb-20 ">
+            <div
+              style={{ backgroundImage: 'url("/img/sneakers/2.jpg")' }}
+              className="cartItemImg"></div>
+            <div className="mr-20 flex">
+              <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+              <b>2 999 грн</b>
+            </div>
+            <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
+          </div>
+          <div className="cartItem d-flex align-center mb-20 ">
+            <div
+              style={{ backgroundImage: 'url("/img/sneakers/4.jpg")' }}
+              className="cartItemImg"></div>
+            <div className="mr-20 flex">
+              <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+              <b>2 999 грн</b>
+            </div>
+            <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
+          </div>
+        </div>
+
+        <div className="cartTotalBlock">
+          <ul>
+            <li className="d-flex">
+              <span>Итого:</span>
+              <div></div>
+              <b>4498 грн</b>
+            </li>
+            <li className="d-flex">
+              <span>Налог 5%:</span>
+              <div></div>
+              <b>274 грн</b>
+            </li>
+          </ul>
+
+          <button className="greenButton">
+            Оформить заказ
+            <img src="img/arrow.svg" alt="Arrow" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Drawer;
---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+function Header() {
+  return (
+    <header className="d-flex justify-between p-40 ">
+      <div className="align-center d-flex ">
+        <img width={40} height={40} src="/img/logo.png" />
+        <div>
+          <h3 className="text-uppercase">React Sneakers</h3>
+          <p className="opacity-5">Магазин лучших кроссовок</p>
+        </div>
+      </div>
+      <ul className="headerRight d-flex align-center ">
+        <li className="mr-30">
+          <img width={18} height={18} src="/img/basket.svg" />
+          <span>5420 грн</span>
+        </li>
+        <li>
+          <img width={18} height={18} src="/img/user.svg" />
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header
